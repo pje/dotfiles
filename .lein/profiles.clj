c0c1bca5d063eec5ae8ef707ef52e297da8648f8
Patrick Ellis <patrick@soundcloud.com>
2016-04-12 17:42:35 -0400

    remove whidbey dependency from lein/profile

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,18 +1,3 @@
 {:user
   {:repl-options
-    {:prompt (fn [ns] (str "(ns " ns ") λ "))}
-    :plugins [[mvxcvi/whidbey "1.0.0"]]
-      :whidbey
-      {:color-scheme
-        {:delimiter       nil
-         :tag             [:red]
-         :nil             [:bold :black]
-         :boolean         [:bold :blue]
-         :number          [:magenta]
-         :string          [:bold :green]
-         :character       [:bold :green]
-         :keyword         [:bold :yellow]
-         :symbol          [:bold :cyan]
-         :function-symbol [:bold :red]
-         :class-delimiter [:blue]
-         :class-name      [:bold :yellow]}}}}
+    {:prompt (fn [ns] (str "(ns " ns ") λ "))}}}
