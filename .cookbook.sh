3071fd1f09eeaf2218b6ccf83496363a9c2940a9
Patrick Ellis <patrick.j.ellis@gmail.com>
2018-04-08 17:38:04 -0400

    add .cookbook.sh

---
+++ .cookbook.sh
@@ -0,0 +1,13 @@
+# braindump of things I can never remember
+
+# sort all directories by size
+find . -mindepth 1 -maxdepth 1 -type d | parallel du -s | sort -rn
+
+# list all open ports
+sudo lsof -i -P | grep -i "listen"
+
+# like strace for open files, without slowing down target
+sudo opensnoop -p
+
+# unix timestamp
+date +%s
