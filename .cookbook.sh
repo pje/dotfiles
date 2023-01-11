2d7dde698b79b3186e8376b76c742f7dbfa4e662
Patrick Ellis <patrick.j.ellis@gmail.com>
2023-01-11 12:15:19 -0500

    update cookbook

--- .cookbook.sh
+++ .cookbook.sh
@@ -2,7 +2,7 @@
 # braindump of things I can never remember
 
 # sort all directories by size
-find . -mindepth 1 -maxdepth 1 -type d | parallel du -s | sort -rn
+find . -mindepth 1 -maxdepth 1 -type d | parallel du -sh | sort -rh
 
 # list all open ports
 sudo lsof -i -P | grep -i "listen"
